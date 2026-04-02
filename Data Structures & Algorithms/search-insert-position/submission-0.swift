2d96e13e1c18ccf9b01e72e11a6184b8df5436b9
Akshay Sarmalkar <76788037+akshaysarmalkar74@users.noreply.github.com>
2026-04-02 15:59:02 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+
+        while left <= right {
+            let midIdx = left + ((right - left) / 2)
+            let midVal = nums[midIdx]
+
+            if midVal == target {
+                return midIdx
+            } else if midVal < target {
+                left = midIdx + 1
+            } else {
+                right = midIdx - 1
+            }
+        }
+
+        return left
+    }
+}
